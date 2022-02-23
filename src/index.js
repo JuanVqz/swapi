3d1159f5eea1477b4d3b7a241a5ae1f688b300bf
Juan Vasquez <juan@logicalbricks.com>
2022-02-22 20:01:06 -0600

    initial commit

---
+++ src/index.js
@@ -0,0 +1,28 @@
+const URL_BASE = "https://swapi.dev/api";
+
+const response = await fetch(`${URL_BASE}/people`);
+const { results } = await response.json();
+console.log(results);
+
+let people = document.querySelector("#people");
+let fragment = new DocumentFragment();
+
+results.forEach(function ({ name, height }) {
+  let tr = document.createElement("tr");
+  tr.innerHTML = `
+     <td>${name}</td>
+     <td>${height}</td>
+  `;
+  fragment.appendChild(tr);
+});
+
+//let body = results.map(({ name, height }) => {
+//  return `
+//    <tr>
+//      <td>${name}</td>
+//      <td>${height}</td>
+//    </tr>
+//  `;
+//});
+
+people.replaceChildren(fragment);
