4264bfdef9758b45f365395b75454a80bc1b10b5
Juan Vasquez <juan@logicalbricks.com>
2022-02-27 21:11:59 -0600

    fix: vite build

--- src/main.js
+++ src/main.js
@@ -2,29 +2,24 @@ import "./css/style.css";
 
 const URL_BASE = "https://swapi.dev/api";
 
-const response = await fetch(`${URL_BASE}/people`);
-const { results } = await response.json();
-console.log(results);
+async function renderPeople() {
+  const response = await fetch(`${URL_BASE}/people`);
+  const { results } = await response.json();
+  console.log(results);
 
-let people = document.querySelector("#people");
-let fragment = new DocumentFragment();
+  let people = document.querySelector("#people");
+  let fragment = new DocumentFragment();
 
-results.forEach(function ({ name, height }) {
-  let tr = document.createElement("tr");
-  tr.innerHTML = `
-     <td>${name}</td>
-     <td>${height}</td>
-  `;
-  fragment.appendChild(tr);
-});
+  results.forEach(function ({ name, height }) {
+    let tr = document.createElement("tr");
+    tr.innerHTML = `
+      <td>${name}</td>
+      <td>${height}</td>
+    `;
+    fragment.appendChild(tr);
+  });
 
-//let body = results.map(({ name, height }) => {
-//  return `
-//    <tr>
-//      <td>${name}</td>
-//      <td>${height}</td>
-//    </tr>
-//  `;
-//});
+  people.replaceChildren(fragment);
+}
 
-people.replaceChildren(fragment);
+renderPeople();
